7c2c718e4aa4ac0ec52c8cf4c41e6bd5afca55ff
GDJ34 <GDJ34@DESKTOP-AFP0URD>
2021-04-12 15:22:00 +0900

    DBUtil.getConnection(), DBUtil.close() 메서드 추가, EbookDao.selectEbookListByPage() 메서드 추가

---
+++ mall_client/src/mall/client/commons/DBUtil.java
@@ -0,0 +1,38 @@
+package mall.client.commons;
+
+import java.sql.*;
+
+public class DBUtil {
+	// 1. DB연결
+	public Connection getConnection() {
+		Connection conn = null; //초기화
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/mall","root","java1004");
+		} catch(Exception e) { 		// 예외가나타나면
+			e.printStackTrace();	// 예외메세지출력
+		}
+		return conn;
+	}
+	// 2. DB지원(conn, stmt, rs) 해제
+	public void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+		// throws Exception 안쓰고 try catch절 사용
+		try {
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ mall_client/src/mall/client/model/EbookDao.java
@@ -0,0 +1,42 @@
+package mall.client.model;
+
+import java.sql.*;
+import java.util.*;
+
+import mall.client.commons.DBUtil;
+import mall.client.vo.*;
+
+public class EbookDao {
+	private DBUtil dbutil; 
+	public List<Ebook> selectEbookListByPage(int beginRow, int rowPerPage) {
+		List<Ebook> list = new ArrayList<>();
+		this.dbutil = new DBUtil();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+		String sql = "SELECT ebook_title ebookTitle, ebook_price ebookPrice, ebook_img ebookImg FROM ebook ORDER BY ebook_date DESC LIMIT ?,?";
+		conn = this.dbutil.getConnection();
+		stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, beginRow);
+		stmt.setInt(2, rowPerPage);
+		rs = stmt.executeQuery();
+		
+		while(rs.next()) {
+			Ebook ebook = new Ebook();
+			ebook.setEbookTitle(rs.getString("ebookTitle"));
+			ebook.setEbookPrice(rs.getInt("ebookPrice"));
+			ebook.setEbookImg(rs.getString("ebookImg"));
+			list.add(ebook);
+		}
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally { // 마지막은 finally 실행
+			this.dbutil.close(rs, stmt, conn);
+		}
+		
+		return list;
+	}
+}
